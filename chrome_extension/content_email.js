b113363e876f13b13a3ff1fcb33333fca6fe8ddc
Emma Pollard <110038554+emmapollard154@users.noreply.github.com>
2025-06-18 11:06:23 +0100

    Update email content script
    Want all email browser functionality within chrome extension directory

--- chrome_extension/content_email.js
+++ chrome_extension/content_email.js
@@ -4,16 +4,15 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     console.log('content_email.js received message from:', sender.id, 'with data:', request);
 
     if (request.action === 'clickedOnEmail') { 
+
         const receivedData = request;
-        console.log('Content Script: Processing data from side_panel:', receivedData);
-
-        window.postMessage({
-            type: 'EMAIL_CLICK',
-            payload: receivedData
-        }, 'http://localhost:5174'); // email browser origin
-
-        // send response to side_panel
+        
+        document.addEventListener('click', function(event) {
+            console.log('Click detected!', event.target);
+            // You can get details like event.clientX, event.clientY, event.button, etc.
+        });
         sendResponse({ status: 'content_script_received_and_processed', dataProcessed: receivedData });
+
     }
 });
 
