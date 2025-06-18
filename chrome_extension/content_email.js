a436877ed2ac1e89fd22b8eb3e18e6d0337db85d
Emma Pollard <110038554+emmapollard154@users.noreply.github.com>
2025-06-18 13:40:54 +0100

    Detect button release

--- chrome_extension/content_email.js
+++ chrome_extension/content_email.js
@@ -5,22 +5,42 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
     if (request.action === 'clickedOnEmail') { 
 
-        // const receivedData = request;
+        const eventDetected = "onEmailPage"
 
         document.addEventListener('mousedown', function(event) {
             console.log('Mouse down detected in email browser', event.target);
 
             if (event.target.matches("button")) {
+                const eventDetected = "button pressed"
                 console.log("BUTTON PRESSED IN EMAIL BROWSER")
             }
 
             if (event.target.matches("a")) {
+                const eventDetected = "link pressed"
                 console.log("LINK PRESSED IN EMAIL BROWSER")
             }
 
         });
-        // sendResponse({ status: 'content_script_received_and_processed', dataProcessed: receivedData });
-        sendResponse({ status: 'content_script_received_and_processed'});
+
+
+        document.addEventListener('mouseup', function(event) {
+            console.log('Mouse up detected in email browser', event.target);
+
+            if (event.target.matches("button")) {
+                const eventDetected = "button released"
+                console.log("BUTTON RELEASED IN EMAIL BROWSER")
+            }
+
+            if (event.target.matches("a")) {
+                const eventDetected = "link released"
+                console.log("LINK RELEASED IN EMAIL BROWSER")
+            }
+
+        });
+
+
+
+        sendResponse({ status: 'content_script_received_and_processed', eventDetected: eventDetected});
     }
 });
 
