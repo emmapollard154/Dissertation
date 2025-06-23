03f897c8daa500ebacf1c0cd7e4510f10c356dcc
Emma Pollard <110038554+emmapollard154@users.noreply.github.com>
2025-06-23 14:43:20 +0100

    Tidy up
    Delete extension button script (move to side_panel.js). Delete redundant code. Create email announcement variable.

--- chrome_extension/extension_button.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('buttonToDashboard').addEventListener('click', function() {
-    // chrome.runtime.sendMessage({ action: "openDashboard", url: "http://localhost:5173/" });
-    chrome.runtime.sendMessage({ action: "openDashboard"});
-});
